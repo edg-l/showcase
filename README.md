36aecc23c35e14e58a45ad2945a77448231cc7b2
Edgar <git@edgarluque.com>
2020-10-25 20:09:23 +0100

    progress

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # showcase
 A showcase of various websites/forms/etc
+
+Demo: https://edg-l.github.io/showcase
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,37 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: "Montserrat, sans-serif",
+        mono: "'Fira Code', monospace"
+      },
+      fontSize: {
+        xs: "1rem",
+        sm: "1.1rem",
+        base: "1.2rem",
+        xl: "1.3rem",
+        lg: "1.4rem",
+        "2xl": "1.6rem",
+        "3xl": "2rem",
+        "4xl": "2.5rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+      },
+      colors: {
+        background: "#fafaff",
+        primary: "#582c4d",
+        secondary: "#a26769",
+      },
+      container: {
+        padding: {
+          default: '0.2rem',
+          sm: '5rem',
+          lg: '10rem',
+          xl: '15rem',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
